be62080e35ecf36e6866ca2fb47d5cabe5083156
Sean Barbeau <sjbarbeau@gmail.com>
2016-11-13 21:48:50 -0500

    Add more unit tests, which are failing
    * From https://github.com/matthiaszimmermann/EGM96/blob/master/src/test/java/org/matthiaszimmermann/location/egm96/GeoidTest.java#L145

--- src/test/java/com/barbeaudev/geotools/referencing/operation/transform/test/EarthGravitationalModelTest.java
+++ src/test/java/com/barbeaudev/geotools/referencing/operation/transform/test/EarthGravitationalModelTest.java
@@ -24,12 +24,32 @@ import org.junit.Test;
  */
 public class EarthGravitationalModelTest extends TestCase {
 
+    static final double EPSILON = 0.001;
+
     @Test
     public void testEGM() throws Exception {
         final EarthGravitationalModel gh = new EarthGravitationalModel();
         gh.load("/egm180.nor");
-        assertEquals(1.505, gh.heightOffset(45, 45, 0), 0.001);
-        assertEquals(1.515, gh.heightOffset(45, 45, 1000), 0.001);
-        assertEquals(46.908, gh.heightOffset(0, 45, 0), 0.001);
+
+        // From GeoTools EGM unit test
+        assertEquals(1.505, gh.heightOffset(45, 45, 0), EPSILON);
+        assertEquals(1.515, gh.heightOffset(45, 45, 1000), EPSILON);
+        assertEquals(46.908, gh.heightOffset(0, 45, 0), EPSILON);
+
+        // From https://github.com/matthiaszimmermann/EGM96/blob/master/src/test/java/org/matthiaszimmermann/location/egm96/GeoidTest.java#L145
+        assertEquals(13.89, gh.heightOffset(89.74, 5.75, 0), EPSILON);
+        assertEquals(11.76, gh.heightOffset(86.5, 217.25, 0), EPSILON);
+        assertEquals(43.56, gh.heightOffset(71.25, 8.0, 0), EPSILON);
+        assertEquals(2.35, gh.heightOffset(54.0, 182.25, 0), EPSILON);
+        assertEquals(-12.91, gh.heightOffset(35.5, 186.0, 0), EPSILON);
+        assertEquals(-8.07, gh.heightOffset(14.75, 213.5, 0), EPSILON);
+        assertEquals(-10.18, gh.heightOffset(-22.25, 221.5, 0), EPSILON);
+        assertEquals(25.61, gh.heightOffset(-37.5, 13.0, 0), EPSILON);
+        assertEquals(-17.88, gh.heightOffset(-51.75, 157.25, 0), EPSILON);
+        assertEquals(5.18, gh.heightOffset(-70.5, 346.0, 0), EPSILON);
+        assertEquals(17.5, gh.heightOffset(-74.5, 22.0, 0), EPSILON);
+        assertEquals(-25.71, gh.heightOffset(-84.5, 306.25, 0), EPSILON);
+        assertEquals(-21.81, gh.heightOffset(-86.75, 328.75, 0), EPSILON);
+        assertEquals(-29.79, gh.heightOffset(-90.0, 0.0, 0), EPSILON);
     }
 }
